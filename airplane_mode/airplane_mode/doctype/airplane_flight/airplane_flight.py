8996bea539b3af725b12fb498e6cf9bb0ad48da3
Sambhav Saxena <sambhav.saxena@ambibuzz.com>
2024-12-07 12:38:13 +0530

    completed task 1 of day 4
    Signed-off-by: Sambhav Saxena <sambhav.saxena@ambibuzz.com>

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,12 +1,22 @@
 # Copyright (c) 2024, Ambibuzz Technologies LLP and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class AirplaneFlight(WebsiteGenerator):
 
-    def on_submit(self):
-        self.update_status_to_completed()
+	def on_submit(self):
+		self.status = "Completed"
 
-    def update_status_to_completed(self):
-        self.status = "Completed"
+	def on_cancle(self):
+		self.status='Cancelled'
+
+	def update_gate_number(doc, method):
+		new_gate_number = doc.gate_number
+		tickets = frappe.get_all("Airplane Ticket", filters={"flight": doc.name}, fields=["name"])
+		for ticket in tickets:
+			ticket_doc = frappe.get_doc("Airplane Ticket", ticket["name"])
+			ticket_doc.gate_number = new_gate_number
+			ticket_doc.save()
+		frappe.msgprint(f"Gate number updated in all linked tickets for flight {doc.name}.")
